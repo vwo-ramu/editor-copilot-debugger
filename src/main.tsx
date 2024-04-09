f595e1fb59b5e670aa9815064b187bf1de0b0676
Bhargava Ramu <bhargava.ramu@wingify.com>
2024-04-09 14:40:02 +0530

    chore: add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { NextUIProvider } from "@nextui-org/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { NextUIProvider } from '@nextui-org/react';
 
-import App from "./App.tsx";
-import "./index.css";
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <NextUIProvider>
             <App />
         </NextUIProvider>
-    </React.StrictMode>
+    </React.StrictMode>,
 );
