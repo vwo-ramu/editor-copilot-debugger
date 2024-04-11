10606cc4fd9e65eb24e81944545a9f619e1ccc6f
Bhargava Ramu <bhargava.ramu@wingify.com>
2024-04-11 09:42:30 +0530

    chore: increase warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 700
+  },
   plugins: [react()],
   base: '/editor-copilot-debugger/'
 })
